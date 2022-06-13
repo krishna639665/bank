77a8d43ec0c051e6d7fed10c8e2cdb3dab555174
krishna639665 <kk5433595@gmail.com>
2022-06-13 13:29:53 +0530

    add admin template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  after_create :assign_default_role
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def assign_default_role
+    self.add_role(:user) if self.roles.blank?
+  end
 end
