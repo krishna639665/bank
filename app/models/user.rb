2b4652dc8fe91d04ee687897473265c7d1e7f6e7
krishna639665 <kk5433595@gmail.com>
2022-06-21 17:19:54 +0530

    Add username login and modify user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,45 @@
 class User < ApplicationRecord
+  attr_writer :login
   has_many :accounts, dependent: :destroy
-  
   after_create :assign_default_role
   rolify
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable, :lockable, :timeoutable, :trackable,
+         authentication_keys: [:login]
+
+
+  VALID_USERNAME_REGEX = /\A(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])/i
+  VALID_PASSWORD = /(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}/
+  validates :username, presence: true, length: {minimum: 8, maximum: 20 },
+         format: { with: VALID_USERNAME_REGEX, message: 'cannot have special characters'  },
+         uniqueness: { case_sensitive: false }
+  validates :password, presence: true, format: {with: VALID_PASSWORD, message: 'week password'}
+
+  
+
+  def login
+    @login || self.username || self.email
+  end
 
   def assign_default_role
     self.add_role(:user) if self.roles.blank?
   end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
 end
